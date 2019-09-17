3b576fdbaaa94f2368589eb8d84f35f6132dc890
Rajika-Kandege <54842482+Rajika-Kandege@users.noreply.github.com>
2019-09-17 16:21:32 +0530

    Update objects file with methods
    Declare a method

--- Objects.java
+++ Objects.java
@@ -13,15 +13,26 @@ public class People {
     int age ;
     String name;
     
+    //create method
+    void myself(){
+    System.out.println("Hi I am " + name);
+    }
     
     public static void main(String[] args){
+        
+        //create objects p1 and p2
     
         People p1 = new People();
         p1.age = 34;
         p1.name = "Lotus";
+        
+        //call method then it will do what has initialize inside the method
+        
+        p1.myself();
         People p2 =  new People();
         p2.age =  56;
         p2.name = "benjamin";
+        p2.myself();
         
         System.out.println(p1.age);
         System.out.println(p2.age);
